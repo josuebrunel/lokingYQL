c2dc4cdd7f0884547225f775d5076f5d6aa9983f
Jacques <gkounkou@gmail.com>
2017-12-25 21:46:47 +0100

    Update errors.py
    Defining the __str__ and __repr__
    The result shall be more interesting :-)

--- myql/errors.py
+++ myql/errors.py
@@ -1,9 +1,14 @@
 class NoTableSelectedError(Exception):
-    '''Error raised when no table has been selected
-    '''
+    
+    '''Error raised when no table has been selected '''
     def __init__(self, msg=None):
         self.msg = msg
 
+    ''' A better __str__ version '''
     def __str__(self):
-        return repr(self.msg)
+        return "NoTableSelectedError('{}')".format(self.msg)
+    
+    ''' defining the special function __repr__ '''
+    def __repr__(self):
+        return '{}'.format(self.msg)
  
